27cc01ae1dabbae9102eac5add317c4b69072bf7
tylerweston <tylerjohnweston@gmail.com>
2019-01-31 09:59:09 -0500

    testing pushing

--- src/bearclaw/Model.java
+++ src/bearclaw/Model.java
@@ -3,5 +3,7 @@ package bearclaw;
 public class Model {
     public Model() {
         // init stuff here
+        // eventually all of our list of tags, loading and parsing data
+        // and associated things will go here
     }
 }
